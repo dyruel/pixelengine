4bda7cec377913a79675a065e9d681c467978bef
morgan <morgan@morgans-MacBook-Pro.local>
2014-10-31 01:08:21 +0100

    Initial XCode commit

---
+++ pixelengine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pixelengine
+//
+//  Created by morgan on 31/10/14.
+//  Copyright (c) 2014 Morgan Chopin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
