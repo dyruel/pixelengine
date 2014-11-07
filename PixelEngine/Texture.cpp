f780707c26735f03a92d8579b090faacbd9081b5
dyruel@msn.com </BalduraN5975>
2014-11-07 16:11:25 +0100

    Mipmap

--- PixelEngine/Texture.cpp
+++ PixelEngine/Texture.cpp
@@ -91,7 +91,8 @@ bool TextureManager::loadTextures(const std::vector<std::string>& files, std::un
 			//	texture.height,
 			//	0, texture.type, GL_UNSIGNED_BYTE,
 			//	texture.imageData);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+//			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
